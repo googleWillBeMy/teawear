b86243af636e54cd91ce97692549633bf048cce8
Priya Tyagi <priyatyagi59127@gmail.com>
2024-08-21 13:47:06 +0530

    Fix accessibility issue by replacing <a> with <button> in Navbar.js

--- src/components/layouts/navbar/Navbar.js
+++ src/components/layouts/navbar/Navbar.js
@@ -2,16 +2,14 @@ import React, { useContext } from "react";
 import './navbar.styles.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from "../../../App";
-import {ReactComponent as Cart} from '../../../assets/cart.svg';
+import { ReactComponent as Cart } from '../../../assets/cart.svg';
 import { getAuth, signOut } from "firebase/auth";
 import app from '../../../firebase/Firebase';
 
 const Navbar = ({ darkTheme, darkText }) => {
-
     const user = useContext(UserContext);
     const auth = getAuth(app);
     const navigate = useNavigate();
-
 
     const handleLogout = () => {
         signOut(auth).then(() => {
@@ -19,7 +17,7 @@ const Navbar = ({ darkTheme, darkText }) => {
         })
         .catch((err) => {
             console.log(err);
-        })
+        });
     }
 
     const showLoginandSignUp = (
@@ -35,20 +33,22 @@ const Navbar = ({ darkTheme, darkText }) => {
         <nav className="nav-links-container">
             <Link to="/" className={`${darkText ? 'nav-links-dark' : 'nav-links'}`}>Home</Link>
             <Link to="/tshirt" className={`${darkText ? 'nav-links-dark' : 'nav-links'}`}>Tshirt</Link>
-            <a onClick={handleLogout} className={`${darkText ? 'nav-links-dark' : 'nav-links'}`}>Logout</a>
+            <button onClick={handleLogout} className={`${darkText ? 'nav-links-dark' : 'nav-links'}`}>
+                Logout
+            </button>
             <Link to="/cart" className="cart-link"><Cart /></Link>
         </nav>
     )
 
     return (
-        <section className={ `navbar-container ${ darkTheme ? 'background-dark relative' : 'background-transparent' } ` }>
+        <section className={`navbar-container ${darkTheme ? 'background-dark relative' : 'background-transparent'}`}>
             <div className="container flex justify-between align-center">
                 <Link to="/" className="logo">Tshirt<span className="text-primary">wear</span></Link>
 
-                {user ? showLogoutAndCart : showLoginandSignUp }
+                {user ? showLogoutAndCart : showLoginandSignUp}
             </div>
         </section>
     )
 }
 
-export default Navbar
+export default Navbar;
